84dfdf54c469a4b39be33b63a6be34050f27dee9
pandawaa <pallakangoding@gmail.com>
2023-12-25 10:52:55 +0800

    feat: setup database mongodb and auth with clerk

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,6 +2,8 @@ import Link from "next/link"
 import Image from "next/image"
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import { Button } from "../ui/button"
+import NavItems from "./NavItems"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
   return (
@@ -15,9 +17,17 @@ const Header = () => {
             height={38}
           />
         </Link>
+
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
+            <MobileNav />
           </SignedIn>
           <SignedOut>
             <Button asChild className="rounded-full bg-purple-700" size="lg">
